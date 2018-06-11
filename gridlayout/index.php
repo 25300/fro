bfb638954850a517ec5fdfc98efe3b67c896ddcf
25300 <34769565+25300@users.noreply.github.com>
2018-06-11 13:47:02 +0200

    Add files via upload

---
+++ gridlayout/index.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>10 soorten patiënten</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>10 soorten patiënten</h1>
+        <p class="tagline">
+            In het ziekenhuis liggen verschillende soorten patiënten. Hier onder vind je er een paar waar je misschien al van wist, <br>maar misschien ook waar je nog niks over weet.
+        </p>
+    </header>
+
+    <section class="orthopedie">
+        <h3 class="nummer">1</h3>
+        <article class="orthopedie_article">
+            <h3>Orthopedie</h3>
+            <p>
+                De afdeling orthopedie houdt zich bezich met patiënten die een aandoening of letsel hebben aan het houdings- en bewegingsapparaat.
+                Je kunt hierbij denken aan iemand die zijn been heeft gebroken.
+            </p>
+            <img src="img/ortho.jpg" alt="Prothese">
+        </article>
+    </section>
+    <section class="intensive">
+        <h3 class="nummer">2</h3>
+        <article>
+            <h3>Intensive Care</h3>
+            <p>
+                Op de afdeling intensive care liggen alle patiënten bij wie één of meerdere belangrijke organen ernstig verstoord of bedreigd worden.
+                Meestal is liggen patiënten niet lang op de intensive care, maar het kan wel voorkomen. De zorg is hier hoogwaardig.
+            </p>
+        </article>
+    </section>
+    <section class="logopedie">
+        <h3 class="nummer">6</h3>
+        <article>
+            <h3>Logopedie</h3>
+            <p>
+                Op de afdeling logopedie komen patiënten die problemen hebben met communicatie of slikken. Zij onderzoeken en behandelen meestal patiënten tijdens een ziekenhuisopname.
+            </p>
+        </article>
+    </section>
+    <section class="podotheraphie">
+        <h3 class="nummer">4</h3>
+        <article>
+            <h3>Podotherapie</h3>
+            <p>
+                Op de afdeling podotherapie worden patiënten met verschillende soorten voetklachten behandeld. Je kan er heen als je bijvoorbeeld scheef loopt, last hebt van platvoeten of een ingegroeide teennagel.
+            </p>
+        </article>
+    </section>
+    <section class="urologie">
+        <h3 class="nummer">5</h3>
+        <article>
+            <h3>Urologie</h3>
+            <p>
+                Bij de afdeling urologie komen patiënten die klachten of een aandoening aan de urinewegen en/of geslachtsorganen hebben.
+                Ze onderzoeken en behandelen afwijkingen aan de nieren, urineleiders, plasbuis, blaas en prostaat.
+            </p>
+        </article>
+    </section>
+    <section class="geriatrie">
+        <h3 class="nummer">3</h3>
+        <article>
+            <h3>Geriatrie</h3>
+            <p>
+                Op de afdeling geriatrie houden ze zich vooral bezig met het voorkomen, vaststellen en behandelen van ziekten bij ouderen.
+                Vaak hebben ouderen last van ouderdomziektes, die zijn vaak een combinatie van lichamelijk, psychische en sociale problemen. Bijvoorbeeld:
+                incontinentie, geheugenstoornissen en onverklaarde achteruitgang in het dagelijks leven.
+            </p>
+        </article>
+    </section>
+    <section class="palliative">
+        <h3 class="nummer">7</h3>
+        <article>
+            <h3>Palliative zorg</h3>
+            <p>
+                Bij de afdeling palliative zorg komen patiënten die hoogstwaarschijnlijk komen te overlijden. Zij krijgen hier het laatste beetje zorg die ze nodig hebben tot dat zij overleden zijn.
+                Bij de palliative zorg gaat het vooral om het verlichten van de pijn op elk vlak, er is dus geen sprake meer van behandeling.
+            </p>
+        </article>
+    </section>
+    <section class="hart">
+        <h3 class="nummer">8</h3>
+        <article>
+            <h3>Hart- en vaatziektes</h3>
+            <p>
+                Hart- en vaatziekten zijn verschillende soorten aandoening. Je hebt bijvoorbeeld patiënten die hartritmestoornissen hebben of een beroerte, maar het kunnen ook patiënten zijn die last hebben van trombose of vernauwig van de slagaderen.
+            </p>
+        </article>
+    </section>
+    <section class="kanker">
+        <h3 class="nummer">9</h3>
+        <article>
+            <h3>Kanker</h3>
+            <p>
+                Zodra er bij iemand kanker wordt geconstateerd wordt er eerst naar de ernst ervan gekeken. Soms is het mogelijk om het kankergezwel operatief te verwijderen in combinatie met chemokuren, de patiënt zal daarna nog wel eens in het jaar langs moeten komen ter controle.
+                Bij sommige patiënten is er geen behandeling mogelijk, er zal dan nog gekeken worden hoeveel tijd er gerekt kan worden. In het laatste stadium kan een kankerpatiënt naar de palliative zorg toe.
+            </p>
+        </article>
+    </section>
+    <section class="neurologisch">
+        <h3 class="nummer">10</h3>
+        <article>
+            <h3>Neurologische aandoening</h3>
+            <p>
+                Op de afdeling neurologie zijn er verschillende soorten patiënten te vinden. De afdeling neurologie houdt zich vooral bezig met het onderzoeken en behandelen van ziektes van het zenuwstelsel en het spierstelsel.
+            </p>
+        </article>
+    </section>
+
+    <!-- Foto-->
+    <div class="img"></div>
+
+    <!-- aside -->
+    <aside>
+        <h3>
+            <span>Meer weten?</span>
+        </h3>
+        <ul>
+            <li>
+                <a href="#">Hoe moet je omgaan met patiënten?</a>
+            </li>
+            <li>
+                <a href="#">EHBO</a>
+            </li>
+            <li>
+                <a href="#">Hartaanvallen</a>
+            </li>
+            <li>
+                <a href="#">Verpleegkundige</a>
+            </li>
+            <li>
+                <a href="#">Artsen</a>
+            </li>
+        </ul>
+    </aside>
+    <footer>
+        <p>&copy;
+            Mediacollege Amsterdam 2018<br>
+            Bron: FRO opdracht periode 4<br>
+            Door: Alisha van Caspel
+        </p>
+        <div id="validatie">
+            <a href="http://validator.w3.org/check?uri=referer" target="_blank" id="copy">
+                <img src="http://blog.boyet.com/blog/files/media/image/valid-html5-blue.png" alt="Valide HTML5" ></a>
+            <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">
+                <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue.gif" alt="Valide CSS">
+            </a>
+        </div>
+    </footer>
+</body>
+</html>
